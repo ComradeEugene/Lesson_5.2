374d78d729de5807a4ea3506f7de48baecfc708f
Eugene <comradeeugene@gmail.com>
2022-01-23 21:56:38 +0300

    first

--- Lesson_5.2/Program.cs
+++ Lesson_5.2/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using static System.Console;
 
 namespace Lesson_5
 {
     class MainClass
     {
+        static string[] StrSplit(string Str)
+        {
+            string[] arr = Str.Split(' ');
+            return arr;
+        }
+
+        static string ReversWords(string Str)
+        {
+            string ReversStr = "";
+            string[] arr = StrSplit(Str);
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                ReversStr += arr[i] + " ";
+            }
+            return ReversStr; 
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Введите предложение");
+            string Str = ReadLine();
+            Str = ReversWords(Str);
+            WriteLine(Str);
+            ReadKey();
         }
     }
 }
